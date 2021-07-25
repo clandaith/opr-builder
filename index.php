ad451b37c9ceae7404a7f5b26728256428f4ddb8
Troy Davidson <clandaith@gmail.com>
2021-07-24 22:11:45 -0600

    Blork

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <?php
 session_start();
 
-require "classes/Blork.php";
+require 'vendor/autoload.php';
+require 'classes/Blork.php';
 
 use classes\Blork;
+use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
+// set the format
+$output = "%message%";
+$formatter = new LineFormatter($output);
+// create a log channel to STDOUT
+$log = new Logger('my_logger');
+$streamHandler = new StreamHandler('php://stdout', Logger::WARNING);
+$streamHandler->setFormatter($formatter);
+$log->pushHandler($streamHandler);
+// test messages
+$log->addWarning("testing 1");
+$log->addWarning("testing 2");
+
 $_SESSION["logged_in"] = true;
+
 
 echo "hello world";
 echo "<br><br>";
