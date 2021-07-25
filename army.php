23f3bd019a8fbb6e812fc414a7d3ffe8fda160e3
Troy Davidson <clandaith@gmail.com>
2021-07-24 20:39:44 -0600

    Blork

--- army.php
+++ army.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-if($_SESSION["logged_in"] != true){
-echo "not logged in.  Later!";
-}else{
-echo "logged in";
+if ($_SESSION["logged_in"] != true) {
+    echo "not logged in.  Later!";
+} else {
+    echo "logged in";
 }
 
 session_destroy();
--- index.php
+++ index.php
@@ -3,8 +3,6 @@ session_start();
 
 require "classes/Blork.php";
 
-//use classes\Blork;
-
 $_SESSION["logged_in"] = true;
 
 echo "hello world";
@@ -12,4 +10,3 @@ echo "<br><br>";
 
 $blork = new \classes\Blork();
 $blork->doSomething();
-
