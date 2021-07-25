b57acad10ff87f3e826320425ae0d8a1f6448561
Troy Davidson <clandaith@gmail.com>
2021-07-24 20:53:36 -0600

    Blork

--- index.php
+++ index.php
@@ -3,10 +3,14 @@ session_start();
 
 require "classes/Blork.php";
 
+use classes\Blork;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
 $_SESSION["logged_in"] = true;
 
 echo "hello world";
 echo "<br><br>";
 
-$blork = new \classes\Blork();
+$blork = new Blork();
 $blork->doSomething();
