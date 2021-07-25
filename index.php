f712c6e9cdc8ea8a9288c9a175199e8d07629978
Troy Davidson <clandaith@gmail.com>
2021-07-24 20:31:57 -0600

    Blork

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-//require "classes/Blork.php";
+use classes\Blork;
 
 $_SESSION["logged_in"] = true;
 
 echo "hello world";
 echo "<br><br>";
 
-$blork = new \classes\Blork();
+$blork = new Blork();
 $blork->doSomething();
 
