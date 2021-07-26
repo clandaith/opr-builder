9cc32ae8b9cf84fde9e966228219424e84581b97
Troy Davidson <clandaith@gmail.com>
2021-07-26 17:32:53 -0600

    Blork

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 use classes\MainController;
 
 require 'vendor/autoload.php';
-//require 'classes/MainController.php';
+require 'classes/MainController.php';
 
 session_start();
 $_SESSION["logged_in"] = true;
